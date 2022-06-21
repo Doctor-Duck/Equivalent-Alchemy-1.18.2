d02f90b8c9bc0999662535d99b26be19eb52ac30
Doctor-Duck <DoctorDuckHQ@gmail.com>
2022-06-21 18:29:50 -0400

    Initial commit

---
+++ src/main/java/net/doctorduck/eqalch/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.doctorduck.eqalch.mixin;
+
+import net.doctorduck.eqalch.EquivalentAlchemy;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		EquivalentAlchemy.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
